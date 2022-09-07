b9161f1d800967a29adb3cf35880a92604015f06
khalid fifel <77207264+kfifel@users.noreply.github.com>
2022-09-07 14:22:15 +0100

    ajouter des modifications sur chalange

--- week_01/MyWork/Challenge 1/chalange_1.cpp
+++ week_01/MyWork/Challenge 1/chalange_1.cpp
@@ -1,4 +1,9 @@
 #include<stdio.h>
 int main(){
-	printf(" Nom : khalid \n Prenom : FIFEL \n Age : 22 ans \n Numero de Telephone : +212682318296");
+	int age;
+	char nom[15], prenom[15], Tele[15];
+	printf("Donner respectivement votre nom, prenom, age et Telephone");
+	scanf("%s %s %d %s", &nom, &prenom, &age, &Tele);
+	printf(" Nom : %s \n Prenom : %s \n Age : 22 %d ans \n Numero de Telephone : %s", nom, prenom, age, Tele);
+return 0;
 }
