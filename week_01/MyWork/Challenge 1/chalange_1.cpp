142c3b65587cacf29a2e0d1e3eab0716607687b2
khalid fifel <77207264+kfifel@users.noreply.github.com>
2022-09-07 14:42:58 +0100

    add some modification

--- week_01/MyWork/Challenge 1/chalange_1.cpp
+++ week_01/MyWork/Challenge 1/chalange_1.cpp
@@ -4,6 +4,7 @@ int main(){
 	char nom[15], prenom[15], Tele[15];
 	printf("Donner respectivement votre nom, prenom, age et Telephone");
 	scanf("%s %s %d %s", &nom, &prenom, &age, &Tele);
-	printf(" Nom : %s \n Prenom : %s \n Age : 22 %d ans \n Numero de Telephone : %s", nom, prenom, age, Tele);
+	printf("-------------------------------------------------\n");
+	printf(" Nom : %s \n Prenom : %s \n Age : %d ans \n Numero de Telephone : %s", nom, prenom, age, Tele);
 return 0;
 }
