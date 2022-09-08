84e45006107bb566cce8385ec14b4546a0a27cb3
khalid fifel <77207264+kfifel@users.noreply.github.com>
2022-09-08 09:59:26 +0100

    Update chellenge_7.cpp
    add float to challenge 7

--- week_01/MyWork/Challenge 7/chellenge_7.cpp
+++ week_01/MyWork/Challenge 7/chellenge_7.cpp
@@ -3,11 +3,11 @@ int main(){
 	int a, b;
 	scanf("%d%d", &a, &b);
 	
-	printf("a+b : %d \n", a+b);
-	printf("a-b : %d\n", a-b);
-	printf("a*b : %d\n", a*b);
+	printf("a+b : %d \n", (float) a+b);
+	printf("a-b : %d\n", (float) a-b);
+	printf("a*b : %d\n", (float) a*b);
 	printf("a/b : %f\n", (float) a/b);
-	printf("a%b : %d\n", a%b);
+	printf("a%b : %d\n", (float) a%b);
 	
 	
 	return 0;
