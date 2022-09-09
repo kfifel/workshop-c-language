daca463714400fef9ea28089f585e727d5003403
khalid fifel <77207264+kfifel@users.noreply.github.com>
2022-09-09 10:13:31 +0100

    add chellenge 6

--- week_01/MyWork2/challenge6/challenge6.cpp
+++ week_01/MyWork2/challenge6/challenge6.cpp
@@ -1,5 +1,12 @@
 #include<stdio.h>
 int main(){
+	int a;
 	
+	printf("saisez votre nombre : ");
+	scanf("%d", &a);
+	
+	if ( a > 0 ) 	  printf("le nombre est positive");
+	else if ( a < 0 ) printf("le nombre est negative");	
+	else 			  printf("le nombre est NULL");
 return 0;
 }
