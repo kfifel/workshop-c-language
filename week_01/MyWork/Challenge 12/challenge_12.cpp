761ce5f59242e14fa72bf06388fa1535eefc25fc
khalid fifel <77207264+kfifel@users.noreply.github.com>
2022-09-09 09:37:32 +0100

    delete boucle while

--- week_01/MyWork/Challenge 12/challenge_12.cpp
+++ week_01/MyWork/Challenge 12/challenge_12.cpp
@@ -4,13 +4,21 @@ int main(){
 	
 	printf("saisez votre nombre");
 	scanf("%d", &number);
-	
-	while(number){
+
+		N = number % 10;
+		number /= 10; 
+		number_inv += N;
+		
 		N = number % 10;
 		number /= 10; 
 		number_inv *= 10;
 		number_inv += N;
-	}
+		
+		N = number % 10;
+		number /= 10; 
+		number_inv *= 10;
+		number_inv += N;
+		
 	printf("%d", number_inv);
 	return 0;
 }
