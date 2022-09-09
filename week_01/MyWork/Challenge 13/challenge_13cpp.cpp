53c94a0a4dba2dd3e03aa884826477aa563e1aa6
khalid fifel <77207264+kfifel@users.noreply.github.com>
2022-09-09 09:18:59 +0100

    modify the challenge 13

--- week_01/MyWork/Challenge 13/challenge_13cpp.cpp
+++ week_01/MyWork/Challenge 13/challenge_13cpp.cpp
@@ -1,27 +1,25 @@
 #include<stdio.h>
-
-
-//int toHixa( int n ) {
-//	int toHixa, a, b;
-//	while( n ) {
-//		
+//
+//int toOctale( int n ) {
+//	int toOctale, rest,a;
+//	while( n>0 ) {
+//		rest = n % 8;
+//		printf("%d\n",rest);
+//		n /= 8;
+//		a = (rest *j) + rest;
+//		toOctale += rest;
 //	}
+//	return toOctale;
 //}
 
-int toOctale( int n ) {
-	int toOctale, rest,a;
-	while( n>0 ) {
-		rest = n % 8;
-		printf("%d\n",rest);
-		n /= 8;
-		a = (rest *j) + rest;
-		toOctale += rest;
-	}
-	return toOctale;
-}
-
 int main() {
-	printf("6 to octale = %d ", toOctale(85) );
+	int a;
+	
+	printf("saissez votre nemuro");
+	scanf("%d", &a);
+	
+	printf("( %d )10 = ( %o )8 \n", a, a) ;
+	printf("( %d )10 = ( %x )16 ", a, a) ;
 	return 0;
 }
 
