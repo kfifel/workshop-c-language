00cc875384cc0fe45b363e64834e48aa990f25be
khalid fifel <77207264+kfifel@users.noreply.github.com>
2022-09-15 09:37:51 +0100

    add challenge 12

--- week_02/MyWork2/challenge12/challenge12.cpp
+++ week_02/MyWork2/challenge12/challenge12.cpp
@@ -1,1 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+void set(int n, int pos, int *T);
+
+int main(){
+	int t[10] = { 1, 2, 3, 4, 6}, *p, pos, n, i;
+	p = &t[0];
+	
+	// affecher le continue de tableau
+	// for index
+	for( i =0; i < 5; i++)
+	printf("| %d ", i+1);
+	printf("|\n");
+	// for ligne
+	for( i =0; i < 5; i++)
+	printf("----");
+	printf("\n");
+	
+	for( i =0; i < 5; i++)
+	printf("| %d ", t[i]);
+	printf("|\n");
+	
+	
+	printf("Inserer un nombre ");scanf("%d", &n);
+	do{	
+	printf("Donner la position de votre insertions entre 1 et 10 : ");
+	scanf("%d", &pos);
+	}while(pos > 10 || pos < 0);
+	
+	set(n, pos-1, t);
+	
+	for( i =0; i < 6; i++)
+	printf("| %d ", t[i]);
+	printf("|\n");
+	
+return 0;	
+}
+
+
+void set(int n, int pos, int *T){
+	int i, cn;
+	
+for(i = pos; i < 6; i++){
+	cn = T[i];
+	T[i] = n;
+	n=T[i+1];
+	T[i+1] = cn;
+	i++;
+}
+	
+	
+	
+}
